80d69a0a6951b4ad08513884c34f79e2bd441cc9
Daniel Sandage <danny@unlabeledmusic.com>
2024-03-12 11:07:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,24 +16,6 @@ def submit_form():
         # Parse form data from the request
         form_data = request.form.to_dict()
 
-        email = form_data.get('Please Enter your Email Address:')
-        ease_of_transition = form_data.get(
-            "How would you rate the ease of transitioning and implementation to TPC's services from your previous payroll provider?")
-        iSolved_software = form_data.get(
-            "How user-friendly do you find iSolved, TPC's HR and payroll software?")
-        previous_payroll_provider = form_data.get(
-            "Who was your previous Payroll Provider?")
-        satisfaction_over_previous = form_data.get(
-            "How would you rate your satisfaction for TPC over your previous payroll provider?")
-        tpc_customer_service = form_data.get(
-            "ow would you rate your experience with TPC's customer service in addressing your inquiries and concerns?")
-        number_of_employees = form_data.get(
-            "How many employees does your company currently process payroll for?")
-        would_you_recommend = form_data.get(
-            "How inclined are you to recommend Grant Stuart and TPC's services to another business?")
-        additional_feedback = form_data.get(
-            "Please share your experience or any additional feedback you have regarding your experience with Grant Stuart and TPC.")
-
         # Return a response to indicate successful processing
         return 'Form data received successfully!', 200
     else:
