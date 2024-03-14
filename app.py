48ff2134cb0491e27d153a342f306ad312d63f59
Daniel Sandage <89650969+dannyxmusic@users.noreply.github.com>
2024-03-14 10:18:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -77,18 +77,18 @@ def submit_form():
         # Print the JSON data
         print(data)
 
-        # Make API request to MongoDB Atlas API
-        response = requests.post(
-            f"{MONGODB_API_URL}/insertOne",
-            json={
-                "dataSource": "testimonialGenerator",
-                "database": "tpc_survey_f1",
-                "collection": "cyclic_server",
-                "document": data
-            },
-            headers={"Content-Type": "application/json",
-                     "api-key": MONGODB_API_KEY}
-        )
+        # # Make API request to MongoDB Atlas API
+        # response = requests.post(
+        #     f"{MONGODB_API_URL}/insertOne",
+        #     json={
+        #         "dataSource": "testimonialGenerator",
+        #         "database": "tpc_survey_f1",
+        #         "collection": "cyclic_server",
+        #         "document": data
+        #     },
+        #     headers={"Content-Type": "application/json",
+        #              "api-key": MONGODB_API_KEY}
+        # )
 
         # Simulate an error response
         return "An error occurred while processing the request.", 500
