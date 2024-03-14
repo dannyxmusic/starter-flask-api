a98ac720ae77a0a80466de1083bffc3b90310b63
Daniel Sandage <89650969+dannyxmusic@users.noreply.github.com>
2024-03-14 11:25:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def submit_form():
         # Update 'data' dictionary with parsed data
         data.update(parsed_data)
 
-        print(mongodb_api_key, mongodb_api_url)
+        print(f"{mongodb_api_url}/insertOne", "api-key =" f"{mongodb_api_key}")
 
         # Make API request to MongoDB Atlas API
         response = requests.post(
