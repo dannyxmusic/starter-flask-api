839f768860e42b5651b378024c5391a27c8a7873
Daniel Sandage <89650969+dannyxmusic@users.noreply.github.com>
2024-03-16 10:19:43 -0500

    updates

--- email_tg.py
+++ email_tg.py
@@ -1,20 +1,23 @@
-from googleapiclient.errors import HttpError
+import os
+import sys
+import base64
 from email.message import EmailMessage
-from pymongo import MongoClient
-from bson import ObjectId
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
-import base64
-import sys
-import os
+from googleapiclient.errors import HttpError
+from pymongo import MongoClient
+from bson import ObjectId
 
+# Gmail credentials
 GMAIL_ACCESS_TOKEN = os.environ.get('GMAIL_ACCESS_TOKEN')
 GMAIL_REFRESH_TOKEN = os.environ.get('GMAIL_REFRESH_TOKEN')
 GMAIL_CLIENT_ID = os.environ.get('GMAIL_CLIENT_ID')
 GMAIL_CLIENT_SECRET = os.environ.get('GMAIL_CLIENT_SECRET')
+
+# MongoDB URI
 MONGO_URI = os.environ.get('MONGO_URI')
 
-# Set up OAuth 2.0 credentials
+# Initialize OAuth 2.0 credentials
 credentials = Credentials(
     GMAIL_ACCESS_TOKEN,
     refresh_token=GMAIL_REFRESH_TOKEN,
@@ -26,17 +29,25 @@ credentials = Credentials(
 # Build the Gmail service
 service = build('gmail', 'v1', credentials=credentials)
 
+# MongoDB client
 client = MongoClient(MONGO_URI)
-
-# Access the database and collection
 db = client['tpc_survey_f1']
 collection = db['cyclic_server']
 collection2 = db['survey1_testimonials']
-object_id = sys.argv[1]
-objectId = ObjectId(object_id)
+
+# Extract content function
 
 
 def extract_content(doc):
+    """
+    Extracts email address and testimonial content from document.
+
+    Args:
+        doc (dict): The document from MongoDB.
+
+    Returns:
+        tuple: Email address and testimonial content.
+    """
     conversation_history = doc.get('conversationHistory', {})
     email_address = doc.get('email', '')
     content_keys = ["content8", "content10", "content12"]
@@ -45,49 +56,42 @@ def extract_content(doc):
     return email_address, content_values
 
 
-# Find document with the current random ID
-document = collection.find_one({"_id": objectId})
+# Find document with the provided object ID
+object_id = sys.argv[1]
+object_id = ObjectId(object_id)
+document = collection.find_one({"_id": object_id})
 
-# Extract the content from the document
+# Extract content from the document
 email_address, content = extract_content(document)
 
+# Construct the email message
 first_item = content[0]
 second_item = content[1]
 third_item = content[2]
 
-message = (
+message_body = (
     f"Email: {email_address}\n \n"
     f"Short Testimonial: {first_item}\n \n"
     f"Medium Testimonial: {second_item}\n \n"
     f"Long Testimonial: {third_item}"
 )
 
+message = EmailMessage()
+message.set_content(message_body)
+message["To"] = "sandagedaniel@gmail.com"
+message["From"] = "therealdannyx@gmail.com"
+message["Subject"] = "Testimonial"
 
-def send_email(service, data):
-    try:
-        # Construct the email message
-        message = EmailMessage()
-        message.set_content(str(data))
-        message["To"] = "sandagedaniel@gmail.com"
-        message["From"] = "therealdannyx@gmail.com"
-        message["Subject"] = "test"
+# Encode the message
+encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
 
-        # Encode the message
-        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
-
-        # Send the email
-        send_message = (
-            service.users().messages()
-            .send(userId="me", body={"raw": encoded_message})
-            .execute()
-        )
-
-        print(f'Message Id: {send_message["id"]}')
-        return send_message
-
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-        return None
-
-
-send_email(service, message)
+# Send the email
+try:
+    send_message = (
+        service.users().messages()
+        .send(userId="me", body={"raw": encoded_message})
+        .execute()
+    )
+    print(f'Message Id: {send_message["id"]}')
+except HttpError as error:
+    print(f"An error occurred: {error}")
--- openai_tg.py
+++ openai_tg.py
@@ -1,18 +1,19 @@
-from operator import itemgetter
+import json
+import logging
+import os
+import re
+import subprocess
+import sys
 import random
+
+from operator import itemgetter
+from bson import ObjectId
 from langchain.memory import ConversationBufferMemory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain_openai import OpenAI
 from langchain_core.messages import HumanMessage, AIMessage
-from bson import ObjectId
-import json
-import re
-import os
-import sys
 from pymongo import MongoClient
-import logging
-import subprocess
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -26,32 +27,44 @@ client = MongoClient(MONGO_URI)
 db = client['tpc_survey_f1']
 collection = db['cyclic_server']
 collection2 = db['survey1_testimonials']
-object_id_str = sys.argv[1]
-object_id = ObjectId(object_id_str)
 
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+# Path to the email.py script
 EMAIL_SCRIPT_PATH = 'email_tg.py'
 
+# Path to the OpenAI API key
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+
+# Initialize OpenAI instance
 llm = OpenAI(openai_api_key=OPENAI_API_KEY)
 
 
 def process_openai(insert_id, data):
-    # Convert insert_id to ObjectId
+    """
+    Process data using OpenAI.
+
+    Args:
+        insert_id (str): The ID of the document.
+        data (dict): The data to process.
+    """
     try:
+        # Convert insert_id to ObjectId
         insert_id = ObjectId(insert_id)
     except Exception as e:
-        print(f"Error converting insert_id to ObjectId: {e}")
+        logger.error(f"Error converting insert_id to ObjectId: {e}")
         sys.exit(1)
 
     data_from_db = collection.find_one({"_id": insert_id})
+
+    if not data_from_db:
+        logger.error(f"Document with _id {insert_id} not found.")
+        sys.exit(1)
 
     # Retrieve all "_id" values from the collection
     all_ids = [doc["_id"] for doc in collection2.find({}, {"_id": 1})]
 
     # Randomly select two "_id" values from the list
     random_ids = random.sample(all_ids, 2)
-
-    logging.info(f"Randomly selected IDs: {random_ids}")
+    logger.info(f"Randomly selected IDs: {random_ids}")
 
     def extract_content(doc):
         return ' '.join([doc[key] for key in ["content8", "content10", "content12"]])
@@ -67,98 +80,70 @@ def process_openai(insert_id, data):
         # Append the content to the list
         contents.append(content)
 
-    if not data_from_db:
-        print(f"Document with _id {insert_id} not found.")
-        sys.exit(1)
-
     prompt = ChatPromptTemplate.from_messages(
-        [
-            ("ai", "you are a helpful chatbot"),
-            MessagesPlaceholder(variable_name="history"),
-            ("human", "{input}"),
-        ]
+        [("ai", "you are a helpful chatbot"),
+         MessagesPlaceholder(variable_name="history"),
+         ("human", "{input}")]
     )
 
     prompt2 = ChatPromptTemplate.from_messages(
-        [
-            ("ai", "analyze the sentiment and return only a numerical value"),
-            MessagesPlaceholder(variable_name="history"),
-            ("human", "{input}"),
-        ]
+        [("ai", "analyze the sentiment and return only a numerical value"),
+         MessagesPlaceholder(variable_name="history"),
+         ("human", "{input}")]
     )
 
     memory = ConversationBufferMemory(return_messages=True)
-
     memory.load_memory_variables({})
 
-    chain = (
-        RunnablePassthrough.assign(
-            history=RunnableLambda(
-                memory.load_memory_variables) | itemgetter("history")
-        )
-        | prompt
-        | llm
-    )
+    chain = (RunnablePassthrough.assign(
+        history=RunnableLambda(
+            memory.load_memory_variables) | itemgetter("history")) | prompt | llm)
 
-    chain2 = (
-        RunnablePassthrough.assign(
-            history=RunnableLambda(
-                memory.load_memory_variables) | itemgetter("history")
-        )
-        | prompt2
-        | llm
-    )
+    chain2 = (RunnablePassthrough.assign(
+        history=RunnableLambda(
+            memory.load_memory_variables) | itemgetter("history")) | prompt2 | llm)
 
     inputs = {"input": ("Please review this survey data: " f"{data}")}
     response = chain.invoke(inputs)
-
     memory.save_context(inputs, {"output": response})
 
-    history = memory.load_memory_variables({})
-
-    inputs = {
-        "input": "Gauge the sentiment and normalize on a scale of 0-1.0. The response to (enter your email), (how many employees), and (please add additional feedback) are all user input. However all other survey questions are rated from very difficult to very easy, dissatisfied to very satisfied, and unlikely to very likely. Please refer to the survey responses and estimate a normalized sentiment value. Return the numerical value you've determined and no other context."}
+    inputs = {"input": "Gauge the sentiment and normalize on a scale of 0-1.0. "
+              "The response to (enter your email), (how many employees), "
+              "and (please add additional feedback) are all user input. "
+              "However all other survey questions are rated from very difficult "
+              "to very easy, dissatisfied to very satisfied, and unlikely to very "
+              "likely. Please refer to the survey responses and estimate a normalized "
+              "sentiment value. Return the numerical value you've determined and no other context."}
     response = chain2.invoke(inputs)
-
     memory.save_context(inputs, {"output": response})
 
-    memory.load_memory_variables({})
-
-    inputs = {
-        "input": "Review previous testimonials, notate recurring phrases or language. Prevous testimonies" f"{contents}"}
+    inputs = {"input": "Review previous testimonials, notate recurring phrases or language. "
+              "Previous testimonies" f"{contents}"}
     response = chain.invoke(inputs)
-
     memory.save_context(inputs, {"output": response})
 
-    memory.load_memory_variables({})
-
-    inputs = {
-        "input": "Write a testimonial from the perspective of the person who completed the survey. The testimonial should be 30-50 words long. Do not state that you recently completed a survey. Avoid using phrasing from the recurring phrases review. Each testimony should be unique while portraying the correct sentiment."}
-
+    inputs = {"input": "Write a testimonial from the perspective of the person who completed the survey. "
+              "The testimonial should be 30-50 words long. Do not state that you recently "
+              "completed a survey. Avoid using phrasing from the recurring phrases review. "
+              "Each testimony should be unique while portraying the correct sentiment."}
     response = chain.invoke(inputs)
-
     memory.save_context(inputs, {"output": response})
 
-    memory.load_memory_variables({})
-
-    inputs = {
-        "input": "Write a testimonial from the perspective of the person who completed the survey. This testimonial should be 60-80 words long. Do not state that you recently completed a survey.Avoid using phrasing from the recurring phrases review. Each testimony should be unique while portraying the correct sentiment."}
-
+    inputs = {"input": "Write a testimonial from the perspective of the person who completed the survey. "
+              "This testimonial should be 60-80 words long. Do not state that you recently "
+              "completed a survey. Avoid using phrasing from the recurring phrases review. "
+              "Each testimony should be unique while portraying the correct sentiment."}
     response = chain.invoke(inputs)
-
     memory.save_context(inputs, {"output": response})
 
-    memory.load_memory_variables({})
-
-    inputs = {
-        "input": "Write a testimonial from the perspective of the person who completed the survey. The testimonial should be 100 words or longer. Do not state that you recently completed a survey. Avoid using phrasing from the recurring phrases review. Each testimony should be unique while portraying the correct sentiment."}
-
+    inputs = {"input": "Write a testimonial from the perspective of the person who completed the survey. "
+              "The testimonial should be 100 words or longer. Do not state that you recently "
+              "completed a survey. Avoid using phrasing from the recurring phrases review. "
+              "Each testimony should be unique while portraying the correct sentiment."}
     response = chain.invoke(inputs)
-
     memory.save_context(inputs, {"output": response})
 
     history = memory.load_memory_variables({})
-
     conversationHistory = []
 
     for i, message in enumerate(history['history'], start=1):
@@ -171,9 +156,7 @@ def process_openai(insert_id, data):
             conversationHistory.append({content_key: f"{content}"})
 
     conversationHistory_json = json.dumps(conversationHistory, indent=2)
-
     conversation_history = json.loads(conversationHistory_json)
-
     cleaned_history = {}
 
     # Iterate over each item in the conversation history
@@ -195,13 +178,21 @@ def process_openai(insert_id, data):
         # Update the original document with conversation history
         collection.update_one({"_id": insert_id}, {
                               "$set": {"conversationHistory": cleaned_history}})
-
     except Exception as e:
         logger.error(f"Error updating conversation history: {e}")
 
 
 def update_testimonials(insert_id):
-    result = collection.find_one({"_id": object_id})
+    """
+    Update testimonials.
+
+    Args:
+        insert_id (str): The ID of the document.
+    """
+    # Convert insert_id to ObjectId
+    insert_id = ObjectId(insert_id)
+
+    result = collection.find_one({"_id": insert_id})
 
     filtered_response = {
         "content8": result.get("conversationHistory", {}).get("content8"),
@@ -214,12 +205,12 @@ def update_testimonials(insert_id):
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        print("Usage: python openai_test.py <insert_id> <data>")
+        logger.error("Usage: python openai_test.py <insert_id> <data>")
         sys.exit(1)
     insert_id = sys.argv[1]
+    objectId = ObjectId(insert_id)
     data = json.loads(sys.argv[2])
     process_openai(insert_id, data)
     update_testimonials(insert_id)
     # Call the email.py script
-    email_script_path = 'email.py'  # Adjust this path if necessary
-    subprocess.run(['python', EMAIL_SCRIPT_PATH, object_id_str])
+    subprocess.run(['python', EMAIL_SCRIPT_PATH, insert_id])
