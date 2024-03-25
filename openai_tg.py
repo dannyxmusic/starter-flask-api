371cb143ffae0d394fa7171959a264b909dd3113
Daniel Sandage <89650969+dannyxmusic@users.noreply.github.com>
2024-03-25 10:58:39 -0500

    updates

--- openai_tg.py
+++ openai_tg.py
@@ -11,8 +11,9 @@ from bson import ObjectId
 from langchain.memory import ConversationBufferMemory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.output_parsers import StrOutputParser
 from pymongo import MongoClient
 
 # Configure logging
@@ -28,6 +29,7 @@ db = client['tpc_survey_f1']
 collection = db['cyclic_server']
 collection2 = db['survey1_testimonials']
 
+
 # Path to the email.py script
 EMAIL_SCRIPT_PATH = 'email_tg.py'
 
@@ -35,10 +37,50 @@ EMAIL_SCRIPT_PATH = 'email_tg.py'
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 
 # Initialize OpenAI instance
-llm = OpenAI(openai_api_key=OPENAI_API_KEY)
+model = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.2,
+                   api_key=OPENAI_API_KEY)
+model2 = ChatOpenAI(model='gpt-3.5-turbo', temperature=0.8,
+                    api_key=OPENAI_API_KEY)
+
+# Initialize output parser
+output_parser = StrOutputParser()
 
 
-def process_openai(insert_id, data):
+def append_testimonials(context, summary, short, medium, long, submission_id):
+    """
+    Update testimonials in MongoDB collection.
+
+    Args:
+        context (dict): The context containing conversation history.
+        summary (str): Summary of the testimonials.
+        short (str): Short testimonial.
+        medium (str): Medium-length testimonial.
+        long (str): Long testimonial.
+        submission_id (str): The submission ID associated with the testimonials.
+    """
+    try:
+        serialized_history = [{'type': 'human', 'content': message.content} if isinstance(message, HumanMessage)
+                              else {'type': 'ai', 'content': message.content} for message in context['history']]
+
+        filtered_response = {
+            "submission_id": submission_id,
+            "context": serialized_history,
+            "summary": summary,
+            "short_testimonial": short,
+            "medium_testimonial": medium,
+            "long_testimonial": long,
+        }
+
+        # Insert the document into MongoDB collection
+        collection2.insert_one(filtered_response)
+
+        print("Testimonials updated successfully.")
+    except Exception as e:
+        print(f"Error occurred while updating testimonials: {e}")
+        # Handle the error appropriately (e.g., log the error, return an error message)
+
+
+def process_openai(insert_id):
     """
     Process data using OpenAI.
 
@@ -53,9 +95,20 @@ def process_openai(insert_id, data):
         logger.error(f"Error converting insert_id to ObjectId: {e}")
         sys.exit(1)
 
-    data_from_db = collection.find_one({"_id": insert_id})
+    key1 = "Please share your experience or any additional feedback you have regarding your experience with Grant Stuart and TPC."
+    key2 = "What field or industry does your company specialize in?"
+    key3 = "How many employees does your company currently process payroll for?"
+    key4 = "Who was your previous Payroll Provider?"
 
-    if not data_from_db:
+    data = collection.find_one({"_id": insert_id})
+    survey_responses = data['survey_responses']
+    submission_id = data['submissionID']
+    amt_employees = survey_responses[key3]
+    field_of_work = survey_responses[key2]
+    additional_feedback = survey_responses[key1]
+    prev_provider = survey_responses[key4]
+
+    if not data:
         logger.error(f"Document with _id {insert_id} not found.")
         sys.exit(1)
 
@@ -67,7 +120,7 @@ def process_openai(insert_id, data):
     logger.info(f"Randomly selected IDs: {random_ids}")
 
     def extract_content(doc):
-        return ' '.join([doc[key] for key in ["content8", "content10", "content12"]])
+        return ' '.join([doc[key] for key in ["short_testimonial", "medium_testimonial", "long_testimonial"]])
 
     contents = []
 
@@ -80,104 +133,85 @@ def process_openai(insert_id, data):
         # Append the content to the list
         contents.append(content)
 
-    prompt = ChatPromptTemplate.from_messages(
-        [("ai", "you are a helpful chatbot"),
-         MessagesPlaceholder(variable_name="history"),
-         ("human", "{input}")]
-    )
+    # Run OpenAI models
+    prompt1 = ChatPromptTemplate.from_messages([
+        ("system", "You are an AI designed to assist in testimonial generation. I provide you survey results and you do 2 things; 1. analyze sentiment. 2. Detect recurring wordage or phrasing from previous testimonials."),
+        MessagesPlaceholder(variable_name="history"),
+        ("human", "{input}"),
+    ])
 
-    prompt2 = ChatPromptTemplate.from_messages(
-        [("ai", "analyze the sentiment and return only a numerical value"),
-         MessagesPlaceholder(variable_name="history"),
-         ("human", "{input}")]
-    )
+    prompt2 = ChatPromptTemplate.from_messages([
+        ("system", "You are an AI tool designed to assist in testimonial generation. I provide you survey results and you do 3 things; 1. List all the survey responses from the original object id #. 2. extract any recurring wordage or phrasing from previous testimonials or listed by ai in the conversation history. 3. Briefly summarize the conversation between human and AI for contextual prompt generation."),
+        ("human", "{input}"),
+    ])
+
+    prompt3 = ChatPromptTemplate.from_messages([
+        ("system", "You are an AI tool designed generate a testimonial based on survey results. You do 3 things. Write in first person from the perspective of the customer. 2. Avoid using the provided recurring words and phrases and find alternatives. 3. Generate a unique testimonials"),
+        ("human", "{input}"),
+    ])
 
     memory = ConversationBufferMemory(return_messages=True)
     memory.load_memory_variables({})
 
     chain = (RunnablePassthrough.assign(
-        history=RunnableLambda(
-            memory.load_memory_variables) | itemgetter("history")) | prompt | llm)
+        history=RunnableLambda(memory.load_memory_variables) | itemgetter("history"))
+        | prompt1 | model | output_parser)
 
-    chain2 = (RunnablePassthrough.assign(
-        history=RunnableLambda(
-            memory.load_memory_variables) | itemgetter("history")) | prompt2 | llm)
+    chain2 = prompt2 | model | output_parser
 
-    inputs = {"input": ("Please review this survey data: " f"{data}")}
+    chain3 = (prompt3 | model2 | output_parser)
+
+    inputs = {"input": "What is your purpose?"}
+    response = chain.invoke(inputs)
+
+    memory.save_context(inputs, {"output": response})
+
+    memory.load_memory_variables({})
+
+    inputs = {
+        "input": f"Here is a survey with the '_id': {insert_id}. Please review the survey and confirm that you processed the data: Survey response = {survey_responses}"
+    }
+
     response = chain.invoke(inputs)
     memory.save_context(inputs, {"output": response})
 
-    inputs = {"input": "Gauge the sentiment and normalize on a scale of 0-1.0. "
-              "The response to (enter your email), (how many employees), "
-              "and (please add additional feedback) are all user input. "
-              "However all other survey questions are rated from very difficult "
-              "to very easy, dissatisfied to very satisfied, and unlikely to very "
-              "likely. Please refer to the survey responses and estimate a normalized "
-              "sentiment value. Return the numerical value you've determined and no other context."}
-    response = chain2.invoke(inputs)
-    memory.save_context(inputs, {"output": response})
-
-    inputs = {"input": "Review previous testimonials, notate recurring phrases or language. "
-              "Previous testimonies" f"{contents}"}
-    response = chain.invoke(inputs)
-    memory.save_context(inputs, {"output": response})
-
-    inputs = {"input": "Write a testimonial from the perspective of the person who completed the survey. "
-              "The testimonial should be 30-50 words long. Do not state that you recently "
-              "completed a survey. Avoid using phrasing from the recurring phrases review. "
-              "Each testimony should be unique while portraying the correct sentiment."}
-    response = chain.invoke(inputs)
-    memory.save_context(inputs, {"output": response})
-
-    inputs = {"input": "Write a testimonial from the perspective of the person who completed the survey. "
-              "This testimonial should be 60-80 words long. Do not state that you recently "
-              "completed a survey. Avoid using phrasing from the recurring phrases review. "
-              "Each testimony should be unique while portraying the correct sentiment."}
-    response = chain.invoke(inputs)
-    memory.save_context(inputs, {"output": response})
-
-    inputs = {"input": "Write a testimonial from the perspective of the person who completed the survey. "
-              "The testimonial should be 100 words or longer. Do not state that you recently "
-              "completed a survey. Avoid using phrasing from the recurring phrases review. "
-              "Each testimony should be unique while portraying the correct sentiment."}
+    inputs = {
+        "input": f"Before I ask you to generate a testimonial I'd like you to review these historical testimonial responses for recurring language. Document them and we will avoid using repeat language in our future testimonial generations. Historical Documents = {contents}"
+    }
     response = chain.invoke(inputs)
     memory.save_context(inputs, {"output": response})
 
     history = memory.load_memory_variables({})
-    conversationHistory = []
 
-    for i, message in enumerate(history['history'], start=1):
-        content_key = f"content{i}"
-        if isinstance(message, HumanMessage):
-            content = message.content
-            conversationHistory.append({content_key: f"Human: {content}"})
-        elif isinstance(message, AIMessage):
-            content = message.content
-            conversationHistory.append({content_key: f"{content}"})
+    history = memory.load_memory_variables({})
 
-    conversationHistory_json = json.dumps(conversationHistory, indent=2)
-    conversation_history = json.loads(conversationHistory_json)
-    cleaned_history = {}
+    inputs = {
+        "input": f"Please review the conversation history. conversation_history = {history}"
+    }
+    summary = chain2.invoke(inputs)
 
-    # Iterate over each item in the conversation history
-    for item in conversation_history:
-        key = next(iter(item))  # Get the key of the dictionary
-        # Get the value and remove leading/trailing whitespace
-        value = item[key].strip()
+    input1 = {
+        'input': f'Review the context: context={summary}. \nGenerate a 60-80 word testimonial using the information provided. Incorporate the amount of employees the company has ({amt_employees}). If the previous payroll provider is listed then mention the company ({prev_provider}). If the additional feedback ({additional_feedback}) is negative please reword to have a positive outlook for future improvements. If the additional feedback ({additional_feedback}) is positive please incorporate verbatim the customer\'s wording to retain authenticity of the testimony.'
+    }
 
-        # Check if the key corresponds to content2, content4, content6, content8, or content10
-        if key in ['content2', 'content4', 'content6', 'content8', 'content10']:
-            # Remove any occurrences of 'AI:' followed by whitespace or newline characters
-            value = re.sub(r'AI:\s*', '', value)
-            # Insert 'AI: ' at the beginning
-            value = 'AI: ' + value.strip()
+    medium_testimony = chain3.invoke(input1)
 
-        cleaned_history[key] = value
+    input2 = {
+        'input': f'Review the context: context={summary}. \nGenerate a 30-50 word testimonial using the information provided. Incorporate the amount of employees the company has ({amt_employees}). If the previous payroll provider is listed then mention the company ({prev_provider}). If the additional feedback ({additional_feedback}) is negative please reword to have a positive outlook for future improvements. If the additional feedback ({additional_feedback}) is positive please incorporate verbatim the customer\'s wording to retain authenticity of the testimony.'
+    }
+
+    short_testimony = chain3.invoke(input2)
+
+    input3 = {
+        'input': f'Review the context: context={summary}. \nGenerate a 100-120 word testimonial using the information provided. Incorporate the amount of employees the company has ({amt_employees}). If the previous payroll provider is listed then mention the company ({prev_provider}). If the additional feedback ({additional_feedback}) is negative please reword to have a positive outlook for future improvements. If the additional feedback ({additional_feedback}) is positive please incorporate verbatim the customer\'s wording to retain authenticity of the testimony.'
+    }
+
+    long_testimony = chain3.invoke(input3)
 
     try:
         # Update the original document with conversation history
-        collection.update_one({"_id": insert_id}, {
-                              "$set": {"conversationHistory": cleaned_history}})
+        append_testimonials(context=history, summary=summary, short=short_testimony,
+                            medium=medium_testimony, long=long_testimony, submission_id=submission_id)
     except Exception as e:
         logger.error(f"Error updating conversation history: {e}")
 
@@ -204,13 +238,11 @@ def update_testimonials(insert_id):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 2:
         logger.error("Usage: python openai_test.py <insert_id> <data>")
         sys.exit(1)
     insert_id = sys.argv[1]
-    objectId = ObjectId(insert_id)
-    data = json.loads(sys.argv[2])
-    process_openai(insert_id, data)
-    update_testimonials(insert_id)
+    process_openai(insert_id)
+    # update_testimonials(insert_id)
     # Call the email.py script
     subprocess.run(['python', EMAIL_SCRIPT_PATH, insert_id])
